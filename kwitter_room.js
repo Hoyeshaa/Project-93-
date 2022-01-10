3c3af5541c283cb6b883b012745b4fa44f78b63b
Hoyeshaa <92789345+Hoyeshaa@users.noreply.github.com>
2022-01-10 17:08:43 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -11,11 +11,27 @@ const firebaseConfig = {
     
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
+    username = localStorage.getItem("username");
+    document.getElementById("username").innerHTML = "Welcome "+ username + "!";
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      console.log(Room_names);
+      row = "<div class='room_name'id="+Room_names+" onclick='redirecttoroomname(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML += row;
       //End code
       });});}
 getData();
+function addroom(){
+      username = document.getElementById("roomname").value;
+      firebase.database().ref("/").child(username).update({
+            purpose : "adding user"
+      });
+      localStorage.setItem("roomname",roomname);
+      window.location = "kwitterpage.html";
+}
+function redirecttoroomname(room){
+      console.log(room);
+      localStorage.setItem("roomname",room);
+      window.location = "kwitterpage.html";
+}
